36e239fd2615aa826a3cff44da4d17b4128f84b6
Dimas Dharma Setiawan <marswan27@gmail.com>
2025-09-22 15:58:07 +0700

    [ADD] Menambahkan Snackbar Custom

---
+++ lib/widget/animated_snackbar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:mobile1/theme/app_colors.dart';
+
+enum SnackbarType { info, success, warning, danger }
+
+class AnimatedSnackbar {
+  static void show(
+    BuildContext context,
+    String message, {
+    SnackbarType type = SnackbarType.info,
+  }) {
+    final overlay = Overlay.of(context);
+    late OverlayEntry entry;
+
+    final controller = AnimationController(
+      duration: const Duration(milliseconds: 400),
+      vsync: Navigator.of(context).overlay!,
+    );
+
+    final animation = Tween<Offset>(
+      begin: const Offset(1, 0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    Color bgColor;
+    IconData icon;
+    switch (type) {
+      case SnackbarType.success:
+        bgColor = AppColors.success;
+        icon = Icons.check_circle_rounded;
+        // color = Colors.white;
+        break;
+      case SnackbarType.warning:
+        bgColor = AppColors.warning;
+        icon = Icons.warning_rounded;
+      case SnackbarType.danger:
+        bgColor = AppColors.danger;
+        icon = Icons.dangerous_rounded;
+      case SnackbarType.info:
+        bgColor = AppColors.info;
+        icon = Icons.info_rounded;
+      default:
+        bgColor = AppColors.secondary;
+        icon = Icons.verified_user_rounded;
+    }
+
+    entry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: 50,
+        left: 20,
+        right: 20,
+        child: SlideTransition(
+          position: animation,
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: AppColors.warning,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: [
+                  // Icon(icon, color: Colors.white),
+                  // const SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(),
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                    onPressed: () async {
+                      await controller.reverse();
+                      entry.remove();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+    overlay.insert(entry);
+    controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () async {
+      await controller.reverse();
+      entry.remove();
+    });
+  }
+}
