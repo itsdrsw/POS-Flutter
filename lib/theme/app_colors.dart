50266b7093d7191d425c7a15f4036f8b2fb5cae1
Dimas Dharma Setiawan <marswan27@gmail.com>
2025-09-22 13:09:09 +0700

    [UPADTE] Menambahkan warna theme

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -4,6 +4,9 @@ class AppColors {
   static const Color primary = Color(0xFF589A8D);
   static const Color secondary = Color(0xFF8FC1B5);
   static const Color bold = Color(0xFF265C4B);
+  static const Color warning = Color(0xFFFAA732);
+  static const Color info = Color(0xFF49AFCD);
+  static const Color success = Color(0xFF006DCC);
   static const Color background = Color(0xFFF5F5F5);
-  static const Color danger = Colors.redAccent;
+  static const Color danger = Color(0xFFDA4F49);
 }
